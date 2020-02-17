82be1723f7d978748a53d60e2885455c8e3b9e7a
Yrrah98 <45289544+Yrrah98@users.noreply.github.com>
2020-02-17 14:39:53 +0000

    Added Library

---
+++ COMP3304Session1/A/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageProcessor;
+
+namespace AssignmentClass
+{
+    public class Class1
+    {
+        // DECLARE ImageFactory _imgFactory
+        private ImageFactory _imgFactory;
+
+
+        public Class1()
+        {
+
+            // INSTANTIATE
+            _imgFactory = new ImageFactory(false);
+
+
+            
+        }
+
+    }
+}
